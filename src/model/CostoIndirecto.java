6815b20c77486742f577acf6fce3e5b0221e824c
Jhon Mora <jhonedmora@gmail.com>
2019-11-07 14:39:45 -0500

    More comments made

--- src/model/CostoIndirecto.java
+++ src/model/CostoIndirecto.java
@@ -1,30 +1,27 @@
 package model;
 
+/**Represents all of the indirect costs of the organization.*/
 public class CostoIndirecto {
 	
+	/**The name of this indirect cost.*/
 	private String name;
+	
+	/**The value of this indirect cost.*/
 	private double value;
 	
+	/**Constructor method. Initializes a new instance of this class with the given information.*/
 	public CostoIndirecto(String n, double v) {
 		name = n;
 		value = v;
 	}
 	
-	
+	/**Returns the name of this Indirect Cost.*/
 	public String getName() {
 		return name;
 	}
 	
+	/**Returns the value of this Indirect Cost.*/
 	public double getValue() {
 		return value;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setValue(double value) {
-		this.value = value;
-	}
-
 }
